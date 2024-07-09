c8d6ee0017649b4ee7a948ac25d53bd476551199
herve.le-bars <herve.le-bars@aviation-civile.gouv.fr>
2024-07-09 20:18:42 +0200

    feat: init oidc auth

--- backend/bloom/services/api.py
+++ backend/bloom/services/api.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends, HTTPException
 from fastapi import Request
+from fastapi.security import OAuth2AuthorizationCodeBearer
+
+from jwt import PyJWKClient
+import jwt
+from typing import Annotated
 
 import redis
 import json
@@ -15,6 +20,40 @@ import time
 
 
 app = FastAPI()
+
+oauth_2_scheme = OAuth2AuthorizationCodeBearer(
+    tokenUrl="http://bloom-auth:8080/realms/bloom-test/protocol/openid-connect/token",
+    authorizationUrl="http://bloom-auth:8080/realms/bloom-test/protocol/openid-connect/auth",
+    refreshUrl="http://bloom-auth:8080/realms/bloom-test/protocol/openid-connect/token",
+)
+
+async def valid_access_token(
+    access_token: Annotated[str, Depends(oauth_2_scheme)]
+):
+    url = "http://bloom-auth:8080/realms/bloom-test/protocol/openid-connect/certs"
+    optional_custom_headers = {"User-agent": "custom-user-agent"}
+    jwks_client = PyJWKClient(url, headers=optional_custom_headers)
+
+    try:
+        signing_key = jwks_client.get_signing_key_from_jwt(access_token)
+        data = jwt.decode(
+            access_token,
+            signing_key.key,
+            algorithms=["RS256"],
+            #audience="api",
+            #options={"verify_exp": True},
+        )
+        return data
+    except jwt.exceptions.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+
+@app.get("/public")
+def get_private():
+    return {"message": "Ce endpoint est public"}
+
+@app.get("/private", dependencies=[Depends(valid_access_token)])
+def get_private():
+    return {"message": "Ce endpoint est privé"}
 
 @app.get("/cache/all/flush")
 async def cache_all_flush(request:Request):
